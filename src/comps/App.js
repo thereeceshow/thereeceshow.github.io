fdf230a965bc58faf49edc844eb115aceddca283
Reece Walter <reece@reecewalter.com>
2021-03-29 16:18:13 -0400

    going react

---
+++ src/comps/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css';
+import Splash from './Splash'
+
+function App() {
+  return (
+    <div className="App">
+      <Splash />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comps/Splash.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+function Splash() {
+    return (
+      <div className="Splash bg-dark bg-gradient">
+        Test
+        <br />
+        test
+      </div>
+    );
+  }
+
+  export default Splash;
